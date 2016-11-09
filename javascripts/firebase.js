2ae4bbf4fd5ba159ce0212d59f26780d1c58cfc3
Dani Adkins <danirenaeadkins12@gmail.com>
2016-11-08 19:55:54 -0600

    added js files

---
+++ javascripts/firebase.js
@@ -0,0 +1,19 @@
+// "use strict";
+
+// var NFAPI = (function(){
+//  return {
+//   firebaseCredentials : function(){
+//     return new Promise ((resolve, reject)=>{
+//       $.ajax({
+//         method: 'GET',
+//         url: `apiKeys.json`
+//       }).then((response)=>{
+//         resolve(response);
+//       },(error)=>{
+//         reject(error);
+
+//       });
+//     });
+//   }
+//  };
+// })();
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-alert('shit');
+"use strict";
+
+$(document).ready(function(){
+ NFAPI.firebaseCredentials().then(function(keys){
+   console.log("keys", keys);
+   apiKeys = keys;
+   firebase.initializeApp(apiKeys);
+ });
